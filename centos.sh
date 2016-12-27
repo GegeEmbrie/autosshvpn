73957183f2a9181d48809aed36b3da00561e48f1
GegeEmbrie <Gegefcp@gmail.com>
2016-12-27 19:00:12 +0700

    Add files via upload

---
+++ centos.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+clear
+echo "=============================="
+echo "        Selamat Datang        "
+echo "=============================="
+echo "Ketik 'I' Untuk VPS Non-Lokal"
+echo "Ketik 'L' Untuk VPS Lokal" 
+echo "=============================="
+read -p "Location : " -e loc
+yum update -y
+
+# go to root
+cd
+
+# remove unused
+yum -y remove sendmail;
+yum -y remove httpd;
+yum -y remove cyrus-sasl
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.d/rc.local
+
+# install wget and curl
+yum -y install wget curl
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service sshd restart
+
+# set repo
+wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh epel-release-6-8.noarch.rpm
+rpm -Uvh remi-release-6.rpm
+
+OS=`uname -m`;
+if [ "$OS" == "x86_64" ]; then
+  wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+  rpm -Uvh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+else
+  wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.i686.rpm
+  rpm -Uvh rpmforge-release-0.5.3-1.el6.rf.i686.rpm
+fi
+
+sed -i 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/rpmforge.repo
+sed -i -e "/^\[remi\]/,/^\[.*\]/ s|^\(enabled[ \t]*=[ \t]*0\\)|enabled=1|" /etc/yum.repos.d/remi.repo
+rm -f *.rpm
+
+# update
+yum update -y
+
+# Install Essential Package
+yum -y install wondershaper rrdtool screen iftop htop nmap bc nethogs openvpn vnstat ngrep mtr git zsh mrtg unrar rsyslog rkhunter mrtg net-snmp net-snmp-utils expect nano bind-utils
+yum -y groupinstall 'Development Tools'
+yum -y install cmake
+yum -y --enablerepo=rpmforge install axel sslh ptunnel unrar
+
+# Install Webmin
+yum update && yum groupinstall "Development Tools"
+wget http://prdownloads.sourceforge.net/webadmin/webmin-1.770-1.noarch.rpm
+yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
+rpm -U webmin-1.770-1.noarch.rpm && chkconfig webmin on
+chkconfig webmin on
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
+
+# OpenSSH Setting
+sed -i '/#Port 22/a Port 22' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+service sshd restart
+
+# Install Dropbear
+yum -y install dropbear
+echo "OPTIONS=\"-p 109 -p 110 -p 443 -p 999\"" > /etc/sysconfig/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+cd
+
+chkconfig dropbear on
+
+# Install Webserver Port 81
+yum install nginx php libapache2-mod-php php-fpm php-cli php-mysql php-mcrypt libxml-parser-perl -y
+wget -O /etc/nginx/nginx.conf "http://autoscript.kepalatupai.com/file/nginx.conf"
+sed -i 's/www-data/nginx/g' /etc/nginx/nginx.conf
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+rm /etc/nginx/conf.d/*
+wget -O /etc/nginx/conf.d/vps.conf "http://autoscript.kepalatupai.com/file/vps.conf"
+sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
+chmod -R +rx /home/vps
+wget -O /home/vps/public_html/uptime.php "http://autoscript.kepalatupai.com/uptime.php1"
+wget -O /home/vps/public_html/index.html "http://autoscript.kepalatupai.com/addons/index.html1"
+service php-fpm restart
+service nginx restart
+chkconfig php-fpm on
+chkconfig nginx on
+cd
+
+# Install VNSTAT
+yum install vnstat -y
+cd /home/vps/public_html/
+wget http://autoscript.kepalatupai.com/file/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+if [[ `ifconfig -a | grep "venet0"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0:0"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0:0-00"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0-00"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "eth0"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0:0"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0:0-00"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0-00"` ]]
+then
+cekvirt='KVM'
+fi
+if [ $cekvirt = 'KVM' ]; then
+	sed -i 's/eth0/eth0/g' config.php
+	sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+	sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+	sed -i 's/Internal/Internet/g' config.php
+	sed -i '/SixXS IPv6/d' config.php
+	cd
+elif [ $cekvirt = 'OpenVZ' ]; then
+	sed -i 's/eth0/venet0/g' config.php
+	sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+	sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+	sed -i 's/Internal/Internet/g' config.php
+	sed -i '/SixXS IPv6/d' config.php
+	cd
+else
+	cd
+fi
+
+# Install Fail2Ban
+yum -y install fail2ban;service fail2ban restart
+
+# Install BadVPN
+wget -O /usr/bin/badvpn-udpgw "http://autoscript.kepalatupai.com/file/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "http://autoscript.kepalatupai.com/file/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# Install Squid
+wget http://autoscript.kepalatupai.com/file/squid.sh && bash squid.sh
+
+# Addons
+wget http://autoscript.kepalatupai.com/addons/addons.sh && sh addons.sh
+sed -i 's/1000/500/g' /usr/bin/akun
+
+# OpenVPN
+wget http://autoscript.kepalatupai.com/file/cenovpn.sh && bash cenovpn.sh
+
+# Finishing
+wget -O /etc/vpnfix.sh "http://autoscript.kepalatupai.com/file/vpnfix.sh"
+chmod 777 /etc/vpnfix.sh
+sed -i 's/exit 0//g' /etc/rc.d/rc.local
+echo "" >> /etc/rc.d/rc.local
+echo "bash /etc/vpnfix.sh" >> /etc/rc.d/rc.local
+echo "$ screen badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/null &" >> /etc/rc.d/rc.local
+echo "nohup ./cron.sh &" >> /etc/rc.d/rc.local
+echo "exit 0" >> /etc/rc.d/rc.local
+wget http://autoscript.kepalatupai.com/addons/remove.sh && sh remove.sh
+rm /root/debian.sh
+
+# Log
+clear
+wget http://autoscript.kepalatupai.com/addons/details.sh && bash details.sh
+rm details.sh
+history -c
---
+++ debian.sh
@@ -0,0 +1,419 @@
+#!/bin/bash
+clear
+echo "=============================="
+echo "        Selamat Datang        "
+echo "=============================="
+echo "Ketik 'I' Untuk VPS Non-Lokal"
+echo "Ketik 'L' Untuk VPS Lokal" 
+echo "=============================="
+read -p "Location : " -e loc
+apt-get update
+
+# go to root
+cd
+
+# remove unused
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# set repo
+ver=`cat /etc/debian_version`
+if [ $ver = '6.0' ]
+then
+debver='6'
+elif [ $ver = '6.1' ]
+then
+debver='6'
+elif [ $ver = '6.2' ]
+then
+debver='6'
+elif [ $ver = '6.3' ]
+then
+debver='6'
+elif [ $ver = '6.4' ]
+then
+debver='6'
+elif [ $ver = '6.5' ]
+then
+debver='6'
+elif [ $ver = '6.6' ]
+then
+debver='6'
+elif [ $ver = '6.7' ]
+then
+debver='6'
+elif [ $ver = '6.8' ]
+then
+debver='6'
+elif [ $ver = '6.9' ]
+then
+debver='6'
+elif [ $ver = '7.0' ]
+then
+debver='7'
+elif [ $ver = '7.1' ]
+then
+debver='7'
+elif [ $ver = '7.2' ]
+then
+debver='7'
+elif [ $ver = '7.3' ]
+then
+debver='7'
+elif [ $ver = '7.4' ]
+then
+debver='7'
+elif [ $ver = '7.5' ]
+then
+debver='7'
+elif [ $ver = '7.6' ]
+then
+debver='7'
+elif [ $ver = '7.7' ]
+then
+debver='7'
+elif [ $ver = '7.8' ]
+then
+debver='7'
+elif [ $ver = '7.9' ]
+then
+debver='7'
+elif [ $ver = '8.0' ]
+then
+debver='8'
+elif [ $ver = '8.1' ]
+then
+debver='8'
+elif [ $ver = '8.2' ]
+then
+debver='8'
+elif [ $ver = '8.3' ]
+then
+debver='8'
+elif [ $ver = '8.4' ]
+then
+debver='8'
+elif [ $ver = '8.5' ]
+then
+debver='8'
+elif [ $ver = '8.6' ]
+then
+debver='8'
+elif [ $ver = '8.7' ]
+then
+debver='8'
+elif [ $ver = '8.8' ]
+then
+debver='8'
+elif [ $ver = '8.9' ]
+then
+debver='8'
+else
+debver='Null'
+fi
+if [ $debver = '6' ]; then
+	if [[ "$loc" = "I" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "L" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "i" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "l" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	fi
+elif [ $debver = '7' ]; then
+	if [[ "$loc" = "I" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "L" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "i" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "l" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian7.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	fi
+elif [ $debver = '8' ]; then
+	if [[ "$loc" = "I" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian8"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "L" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian8.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "i" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian8"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	elif [[ "$loc" = "l" ]]; then
+		wget -O /etc/apt/sources.list "http://autoscript.kepalatupai.com/file/sources.list.debian8.lokal"
+		wget "http://autoscript.kepalatupai.com/file/dotdeb.gpg"
+		apt-key add dotdeb.gpg
+		rm dotdeb.gpg
+		apt-get install python-software-properties 
+		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+		cd /root
+		wget http://www.webmin.com/jcameron-key.asc
+		apt-key add jcameron-key.asc
+		cd
+		apt-get update
+	fi
+else
+	cd
+fi
+
+gpg --keyserver pgpkeys.mit.edu --recv-key  9D6D8F6BC857C906      
+gpg -a --export 9D6D8F6BC857C906 | sudo apt-key add -
+gpg --keyserver pgpkeys.mit.edu --recv-key  7638D0442B90D010      
+gpg -a --export 7638D0442B90D010 | sudo apt-key add -
+
+# update
+apt-get update
+
+# Install Essential Package
+apt-get -y install build-essential
+
+# Install Webmin
+apt-get -y install webmin
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
+
+# OpenSSH Setting
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+service ssh restart
+
+# Install Dropbear
+apt-get install zlib1g-dev dpkg-dev dh-make -y
+wget http://autoscript.kepalatupai.com/file/dropbear-2014.63.tar.bz2
+tar jxvf dropbear-2014.63.tar.bz2
+cd dropbear-2014.63
+dpkg-buildpackage
+cd ..
+OS=`uname -m`;
+if [ $OS = 'i686' ]; then
+	dpkg -i dropbear_2014.63-0.1_i386.deb
+elif [ $OS = 'x86_64' ]; then
+	dpkg -i dropbear_2014.63-0.1_amd64.deb
+fi
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 110 -p 109 -p 999"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+cd
+
+# Install Webserver Port 81
+apt-get install nginx php5 libapache2-mod-php5 php5-fpm php5-cli php5-mysql php5-mcrypt libxml-parser-perl -y
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+curl http://autoscript.kepalatupai.com/file/nginx.conf > /etc/nginx/nginx.conf
+curl http://autoscript.kepalatupai.com/file/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/uptime.php "http://autoscript.kepalatupai.com/uptime.php1"
+wget -O /home/vps/public_html/index.html "http://autoscript.kepalatupai.com/addons/index.html1"
+service php5-fpm restart
+service nginx restart
+cd
+
+# Install VNSTAT
+apt-get install vnstat -y
+cd /home/vps/public_html/
+wget http://autoscript.kepalatupai.com/file/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+if [[ `ifconfig -a | grep "venet0"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0:0"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0:0-00"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "venet0-00"` ]]
+then
+cekvirt='OpenVZ'
+elif [[ `ifconfig -a | grep "eth0"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0:0"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0:0-00"` ]]
+then
+cekvirt='KVM'
+elif [[ `ifconfig -a | grep "eth0-00"` ]]
+then
+cekvirt='KVM'
+fi
+if [ $cekvirt = 'KVM' ]; then
+	sed -i 's/eth0/eth0/g' config.php
+	sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+	sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+	sed -i 's/Internal/Internet/g' config.php
+	sed -i '/SixXS IPv6/d' config.php
+	cd
+elif [ $cekvirt = 'OpenVZ' ]; then
+	sed -i 's/eth0/venet0/g' config.php
+	sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+	sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+	sed -i 's/Internal/Internet/g' config.php
+	sed -i '/SixXS IPv6/d' config.php
+	cd
+else
+	cd
+fi
+
+# Install Fail2Ban
+apt-get -y install fail2ban;service fail2ban restart
+
+# Install BadVPN
+apt-get -y install cmake make gcc
+wget http://autoscript.kepalatupai.com/file/badvpn-1.999.127.tar.bz2
+tar xf badvpn-1.999.127.tar.bz2
+mkdir badvpn-build
+cd badvpn-build
+cmake ~/badvpn-1.999.127 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+make install
+screen badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/null &
+cd
+
+# Install Squid
+wget http://autoscript.kepalatupai.com/file/squid.sh && bash squid.sh
+
+# Addons
+wget http://autoscript.kepalatupai.com/addons/addons.sh && sh addons.sh
+
+# OpenVPN
+wget http://autoscript.kepalatupai.com/file/debovpn.sh && bash debovpn.sh
+
+# Finishing
+wget -O /etc/vpnfix.sh "http://autoscript.kepalatupai.com/file/vpnfix.sh"
+chmod 777 /etc/vpnfix.sh
+sed -i 's/exit 0//g' /etc/rc.local
+echo "" >> /etc/rc.local
+echo "bash /etc/vpnfix.sh" >> /etc/rc.local
+echo "$ screen badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/null &" >> /etc/rc.local
+echo "nohup ./cron.sh &" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+wget http://autoscript.kepalatupai.com/addons/remove.sh && sh remove.sh
+rm /root/debian.sh
+
+# Log
+clear
+wget http://autoscript.kepalatupai.com/addons/details.sh && bash details.sh
+rm details.sh
+history -c
