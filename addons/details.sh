022b0609844cd825eda3e2625066b754966aa4ef
Gege Desembri <Gegefcp@gmail.com>
2016-12-28 18:52:38 +0700

    Update get IP

--- addons/details.sh
+++ addons/details.sh
@@ -2,7 +2,7 @@
 clear
 MYIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 if [[ "$MYIP" = "" ]]; then
-		MYIP=$(wget -qO- autoscript.kepalatupai.com/addons/ip.php)
+		MYIP=$(wget -qO- nyobascript.esy.es/ip.php)
 fi
 echo ""  | tee -a log-install.txt
 echo "==============================================================" | tee -a log-install.txt
